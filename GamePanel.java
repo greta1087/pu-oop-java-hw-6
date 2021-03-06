d3c9c4288ba260e21501ad257382970303799d70
Greta <63397200+greta1087@users.noreply.github.com>
2021-03-06 19:37:25 +0200

    Smoko Game
    - Functioning snake
    - Randomly generated food
    - Score
    - Game Over screen

---
+++ GamePanel.java
@@ -0,0 +1,170 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
+    static final int DELAY = 150;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 1;
+    int foodEaten;
+    int foodX;
+    int foodY;
+    int obstacleX;
+    int obstacleY;
+    char direction = 'R';
+    boolean gameRunning = false;
+    Timer timer;
+    Random random;
+
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+    public void startGame() {
+        snakeFood();
+        gameRunning = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g) {
+
+        if(gameRunning) {
+
+            g.setColor(Color.red);
+            g.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
+
+            for(int i = 0; i< bodyParts; i++) {
+                if(i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+                else {
+                    g.setColor(new Color(45,180,0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+            g.setColor(Color.red);
+            g.setFont( new Font("Comic Sans MS",Font.PLAIN, 20));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: "+ (foodEaten * 10), (SCREEN_WIDTH - metrics.stringWidth("Score: "+ foodEaten))/2, g.getFont().getSize());
+        }
+        else {
+            gameOver(g);
+        }
+
+    }
+    public void snakeFood(){
+        foodX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        foodY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+    }
+    public void obstacle(){
+        obstacleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        obstacleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+    }
+    public void move(){
+        for(int i = bodyParts;i>0;i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+
+        switch(direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
+
+    }
+    public void checkSnakeFood() {
+        if((x[0] == foodX) && (y[0] == foodY)) {
+            bodyParts++;
+            foodEaten++;
+            snakeFood();
+        }
+    }
+    public void checkCollisions() {
+        //checks if the snake collides with it's body
+        for(int i = bodyParts;i>0;i--) {
+            if((x[0] == x[i])&& (y[0] == y[i])) {
+                gameRunning = false;
+            }
+        }
+        if(!gameRunning) {
+            timer.stop();
+        }
+    }
+    public void gameOver(Graphics g) {
+        //Score
+        g.setColor(Color.red);
+        g.setFont( new Font("Comic Sans MS",Font.PLAIN, 40));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Score: "+ (foodEaten*10), (SCREEN_WIDTH - metrics1.stringWidth("Score: "+ foodEaten))/2, g.getFont().getSize());
+        //Game Over text
+        g.setColor(Color.red);
+        g.setFont( new Font("Comic Sans MS",Font.PLAIN, 75));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(gameRunning) {
+            move();
+            checkSnakeFood();
+            checkCollisions();
+        }
+        repaint();
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch(e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
+        }
+    }
+
+}
---
+++ SmokoGame.java
@@ -0,0 +1,8 @@
+public class SmokoGame {
+
+    public static void main(String[] args) {
+
+        new GameFrame();
+    }
+
+}
